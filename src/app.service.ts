fb465c820a006b973a9d88bb4747ff9ca6be8177
Billy Huang <10646003@ntub.edu.tw>
2024-08-30 10:34:35 +0800

    feat(app): adjust root response to show all routes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Request } from '@nestjs/common';
+
+import { Request as ExpressRequest, Router } from 'express';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getRoutes(@Request() request: ExpressRequest) {
+    const appRouter = request.app._router as Router;
+    const basePath = `${request.protocol}://${request.get('host')}`;
+    const routes = appRouter.stack
+      .filter((layer) => layer.route)
+      .map((layer) => {
+        const path = layer.route!.path;
+        const method = layer.route!.stack[0].method;
+        return { method: method.toUpperCase(), path: `${basePath}${path}` };
+      });
+    return { routes };
   }
 }
