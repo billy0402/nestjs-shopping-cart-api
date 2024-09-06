billy0402/nestjs-event-api

feat(admin-auth): init

src/apps/admin-auth/admin-auth.controller.ts
@@ -1,0 +1,47 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+
+import { ZodSerializerDto } from 'nestjs-zod';
+
+import {
+  LoginInDto,
+  RefreshInDto,
+  RegisterInDto,
+  TokenPayloadDto,
+  UserInfoDto,
+} from '@/dto/auth.dto';
+import { UsersService } from '@/services/users/users.service';
+
+import { AdminAuthService } from './admin-auth.service';
+
+@ApiTags('admin-auth')
+@Controller('admin/auth')
+export class AdminAuthController {
+  constructor(
+    private readonly adminAuthService: AdminAuthService,
+    private userService: UsersService,
+  ) {}
+
+  @ApiOkResponse({ type: TokenPayloadDto })
+  @HttpCode(HttpStatus.OK)
+  @ZodSerializerDto(TokenPayloadDto)
+  @Post('login')
+  async login(@Body() data: LoginInDto) {
+    return await this.adminAuthService.login(data);
+  }
+
+  @ApiOkResponse({ type: TokenPayloadDto })
+  @HttpCode(HttpStatus.OK)
+  @ZodSerializerDto(TokenPayloadDto)
+  @Post('refresh')
+  async refresh(@Body() data: RefreshInDto) {
+    return await this.adminAuthService.refresh(data.refreshToken);
+  }
+
+  @ApiCreatedResponse({ type: UserInfoDto })
+  @ZodSerializerDto(UserInfoDto)
+  @Post('register')
+  async register(@Body() data: RegisterInDto) {
+    return await this.userService.create(data);
+  }
+}

billy0402/nestjs-shopping-cart-api

src/apps/admin-auth/admin-auth.controller.ts
@@ -1,0 +1,47 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+
+import { ZodSerializerDto } from 'nestjs-zod';
+
+import {
+  LoginInDto,
+  RefreshInDto,
+  RegisterAdminInDto,
+  TokenPayloadDto,
+  UserInfoDto,
+} from '@/dto/auth.dto';
+import { UsersService } from '@/services/users/users.service';
+
+import { AdminAuthService } from './admin-auth.service';
+
+@ApiTags('admin-auth')
+@Controller('admin/auth')
+export class AdminAuthController {
+  constructor(
+    private readonly adminAuthService: AdminAuthService,
+    private userService: UsersService,
+  ) {}
+
+  @ApiOkResponse({ type: TokenPayloadDto })
+  @HttpCode(HttpStatus.OK)
+  @ZodSerializerDto(TokenPayloadDto)
+  @Post('login')
+  async login(@Body() data: LoginInDto) {
+    return await this.adminAuthService.login(data);
+  }
+
+  @ApiOkResponse({ type: TokenPayloadDto })
+  @HttpCode(HttpStatus.OK)
+  @ZodSerializerDto(TokenPayloadDto)
+  @Post('refresh')
+  async refresh(@Body() data: RefreshInDto) {
+    return await this.adminAuthService.refresh(data.refreshToken);
+  }
+
+  @ApiCreatedResponse({ type: UserInfoDto })
+  @ZodSerializerDto(UserInfoDto)
+  @Post('register')
+  async register(@Body() data: RegisterAdminInDto) {
+    return await this.userService.create(data);
+  }
+}